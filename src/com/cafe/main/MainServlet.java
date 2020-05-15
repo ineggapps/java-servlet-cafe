dce952143d823e221d0476023fde87bb404c9689
inegg <inegg.apps@gmail.com>
2020-05-15 14:01:54 +0900

    프로젝트 생성

---
+++ src/com/cafe/main/MainServlet.java
@@ -0,0 +1,32 @@
+package com.cafe.main;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/main/*")
+public class MainServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		process(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		process(req, resp);
+	}
+
+	public static final String VIEWS = "/WEB-INF/views/cafe";
+	private void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String path = VIEWS + "/index.jsp";
+		RequestDispatcher disp = req.getRequestDispatcher(path);
+		disp.forward(req, resp);
+	}
+}
