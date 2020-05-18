f907874612c78ad7ba318cecfa79560e671021af
inegg <inegg.apps@gmail.com>
2020-05-18 12:50:39 +0900

    테이블 작성 (예약어 테이블명 수정 및 초기화 구문 추가)

--- tables.sql
+++ tables.sql
@@ -11,7 +11,7 @@ CREATE TABLE member(
     updated_date DATE DEFAULT SYSDATE, -- 회원정보 수정일시
     phone VARCHAR2(50), -- 휴대폰번호
     enabled NUMBER(1) DEFAULT 1, -- 회원탈퇴여부 (0: 탈퇴, 1:회원(기본값))
-    CONSTRAINT pk_member_num PRIMARY KEY(num)
+    CONSTRAINT pk_member_userNo PRIMARY KEY(userNo)
 );
 
 CREATE SEQUENCE member_seq -- 회원번호 시퀀스 
@@ -54,8 +54,8 @@ CREATE TABLE event(
     end_date DATE NOT NULL, -- 이벤트 종료일자
     created_date DATE DEFAULT SYSDATE, -- 게시글 작성일시
     updated_date DATE DEFAULT SYSDATE, -- 게시글 수정일시
-    CONSTRAINT pk_notice_num PRIMARY KEY(num),
-    CONSTRAINT fk_notice_userNo FOREIGN KEY(userNo) REFERENCES member(userNo)
+    CONSTRAINT pk_event_num PRIMARY KEY(num),
+    CONSTRAINT fk_event_userNo FOREIGN KEY(userNo) REFERENCES member(userNo)
 );
 
 CREATE SEQUENCE event_seq -- 이벤트 게시글 일련번호
@@ -69,9 +69,9 @@ CREATE SEQUENCE event_seq -- 이벤트 게시글 일련번호
 -- 메뉴 구분
 
 CREATE TABLE menu_category(
-    category NUMBER, -- 메뉴 카테고리 구분자 (기본키) (1: 커피, 2: 에이드, 3: 베이커리)
+    categoryNum NUMBER, -- 메뉴 카테고리 구분자 (기본키) (1: 커피, 2: 에이드, 3: 베이커리)
     categoryName VARCHAR2(255), -- 메뉴이름 (커피, 에이드, 베이커리)
-    CONSTRAINT pk_menu_category PRIMARY KEY(category)
+    CONSTRAINT pk_menu_category PRIMARY KEY(categoryNum)
 );
 
 CREATE SEQUENCE menu_category_seq --메뉴 카테고리 구분자 일련번호
@@ -83,13 +83,13 @@ CREATE SEQUENCE menu_category_seq --메뉴 카테고리 구분자 일련번호
 
 
 CREATE TABLE menu( 
-    num NUMBER, -- 메뉴 일련번호 (1: 아메리카노, 2: 크루아상 3: 카페모카 ...)
-    category NUMBER NOT NULL, -- 메뉴 카테고리 구분자
+    menuNum NUMBER, -- 메뉴 일련번호 (1: 아메리카노, 2: 크루아상 3: 카페모카 ...)
+    categoryNum NUMBER NOT NULL, -- 메뉴 카테고리 구분자
     menuName VARCHAR2(255) NOT NULL, -- 메뉴이름 (아메리카노, 크루아상, 카페모카 ...)
     text VARCHAR2(4000) NOT NULL, -- 메뉴 소개글
     price NUMBER DEFAULT 0, -- 가격 (구매 시 필요)
-    CONSTRAINT pk_menu_num PRIMARY KEY(num),
-    CONSTRAINT fk_menu_category FOREIGN KEY(category) REFERENCES menu_category(category)
+    CONSTRAINT pk_menu_num PRIMARY KEY(menuNum),
+    CONSTRAINT fk_menu_category FOREIGN KEY(categoryNum) REFERENCES menu_category(categoryNum)
 );
 
 CREATE SEQUENCE menu_seq -- 메뉴 일련번호 시퀀스
@@ -108,7 +108,7 @@ CREATE TABLE store(
     storeName VARCHAR(200), --매장 이름
     tel VARCHAR(50),
     storeAddress VARCHAR(500), -- 매장 주소
-    CONSTRAINT pk_store_num PRIMARY KEY storeNum
+    CONSTRAINT pk_store_num PRIMARY KEY (storeNum)
 );
 
 CREATE SEQUENCE store_seq -- 매장 일련번호 시퀀스
@@ -122,13 +122,13 @@ CREATE SEQUENCE store_seq -- 매장 일련번호 시퀀스
 ----------------
 --주문과 주문 상세
 
-CREATE order_status(
+CREATE TABLE order_status(
     statusNum NUMBER, -- 1: 결제완료, 2: 제조대기, 3: 제조 중, 4: 제조 완료
     statusName VARCHAR(100),
     CONSTRAINT pk_status_num PRIMARY KEY(statusNum)
 );
 
-CREATE SEQUENCE order_status -- 주문 상태번호
+CREATE SEQUENCE order_status_seq -- 주문 상태번호
     START WITH 1
     INCREMENT BY 1
     NOMAXVALUE
@@ -136,7 +136,7 @@ CREATE SEQUENCE order_status -- 주문 상태번호
     NOCACHE;
 
 
-CREATE TABLE order(
+CREATE TABLE order_history(
     orderNum NUMBER,
     totalPaymentAmount NUMBER NOT NULL, --최종 결제금액
     storeNum NUMBER NOT NULL, --주문 매장번호
@@ -144,12 +144,12 @@ CREATE TABLE order(
     cardNum NUMBER, -- ALTER TABLE로 제약사항 추가하기
     order_date DATE DEFAULT SYSDATE, --주문일시
     cancelNum NUMBER, -- ALTER TABLE로 제약사항 추가하기
-    CONSTRAINT pk_order_num PRIMARY KEY(orderNum),
+    CONSTRAINT pk_order_num PRIMARY KEY(orderNum)
 -- CONSTRAINT fk_cancel_num FOREIGN KEY(cancelNum) REFERENCES order_cancel(cancelNum)
 -- CONSTRAINT fk_cards_num FOREIGN KEY(cardNum) REFERENCES cards(cardNum)
 );
 
-CREATE SEQUENCE order_seq -- 주문 일련번호 시퀀스
+CREATE SEQUENCE order_history_seq -- 주문 일련번호 시퀀스
     START WITH 1
     INCREMENT BY 1
     NOMAXVALUE
@@ -162,7 +162,7 @@ CREATE TABLE order_cancel(
     paymentAmount NUMBER,
     canceled_date DATE DEFAULT SYSDATE,
     CONSTRAINT pk_cancel_num PRIMARY KEY(cancelNum),
-    CONSTRAINT fk_order_num FOREIGN KEY(orderNum) REFERENCES order(orderNum);
+    CONSTRAINT fk_order_cancel_num FOREIGN KEY(orderNum) REFERENCES order_history(orderNum)
 );
 
 CREATE SEQUENCE order_cancel_seq -- 주문 일련번호 시퀀스
@@ -181,8 +181,16 @@ CREATE TABLE order_detail(
     quantity NUMBER NOT NULL, -- 수량
     paymentAmount NUMBER NOT NULL, -- 결제금액 (할인 등 변수 고려)
     CONSTRAINT pk_order_detail_num PRIMARY KEY(detailNum),
-    CONSTRAINT fk_order_num PRIMARY KEY(orderNum) REFERENCES order(orderNum)
+    CONSTRAINT fk_order_detail_num FOREIGN KEY(orderNum) REFERENCES order_history(orderNum)
 );
+
+CREATE SEQUENCE order_detail_seq -- 주문 일련번호 시퀀스
+    START WITH 1
+    INCREMENT BY 1
+    NOMAXVALUE
+    NOCYCLE
+    NOCACHE;
+
 
 
 -- 카드 보유내역
@@ -195,6 +203,14 @@ CREATE TABLE card_model(
     CONSTRAINT pk_model_num PRIMARY KEY(modelNum)
 );
 
+CREATE SEQUENCE card_model_seq -- 주문 일련번호 시퀀스
+    START WITH 1
+    INCREMENT BY 1
+    NOMAXVALUE
+    NOCYCLE
+    NOCACHE;
+
+
 CREATE TABLE cards(
     cardNum NUMBER,
     userNum NUMBER NOT NULL,
@@ -204,7 +220,48 @@ CREATE TABLE cards(
     CONSTRAINT fk_model_num FOREIGN KEY(modelNum) REFERENCES card_model(modelNum)
 );
 
----제약사항 추가
-ALTER TABLE order ADD CONSTRAINT fk_cancel_num FOREIGN KEY(cancelNum) REFERENCES order_cancel(cancelNum);
-ALTER TABLE order ADD CONSTRAINT fk_cards_num FOREIGN KEY(cardNum) REFERENCES cards(cardNum);
+CREATE SEQUENCE cards_seq -- 주문 일련번호 시퀀스
+    START WITH 1
+    INCREMENT BY 1
+    NOMAXVALUE
+    NOCYCLE
+    NOCACHE;
 
+
+---제약사항 추가
+ALTER TABLE order_history ADD CONSTRAINT fk_cancel_num FOREIGN KEY(cancelNum) REFERENCES order_cancel(cancelNum);
+ALTER TABLE order_history ADD CONSTRAINT fk_cards_num FOREIGN KEY(cardNum) REFERENCES cards(cardNum);
+
+
+
+--테이블 삭제
+ALTER TABLE order_history DROP CONSTRAINT fk_cancel_num;
+ALTER TABLE order_history DROP CONSTRAINT fk_cards_num;
+DROP TABLE NOTICE PURGE;
+DROP SEQUENCE NOTICE_SEQ;
+DROP TABLE EVENT PURGE;
+DROP SEQUENCE EVENT_SEQ;
+DROP TABLE MENU PURGE;
+DROP SEQUENCE MENU_SEQ;
+DROP TABLE MENU_CATEGORY PURGE;
+DROP SEQUENCE MENU_CATEGORY_SEQ;
+DROP TABLE STORE PURGE;
+DROP SEQUENCE  STORE_SEQ;
+DROP TABLE ORDER_STATUS PURGE;
+DROP SEQUENCE  ORDER_STATUS_SEQ;
+DROP TABLE ORDER_CANCEL PURGE;
+DROP SEQUENCE ORDER_CANCEL_SEQ;
+DROP TABLE ORDER_DETAIL PURGE;
+DROP SEQUENCE ORDER_DETAIL_SEQ;
+DROP TABLE ORDER_HISTORY PURGE;
+DROP SEQUENCE ORDER_HISTORY_SEQ;
+DROP TABLE CARDS PURGE;
+DROP SEQUENCE CARDS_SEQ;
+DROP TABLE CARD_MODEL PURGE;
+DROP SEQUENCE CARD_MODEL_SEQ;
+DROP TABLE MEMBER PURGE;
+DROP SEQUENCE MEMBER_SEQ;
+
+
+SELECT * FROM TAB;
+SELECT * FROM SEQ;
